0ce1e7a8a85e4e6f4579365fa82144f80e56e935
Tanya Silyutina <48433669+TanyaSilyutina@users.noreply.github.com>
2023-06-03 19:47:14 -0700

    added seeded user

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,14 +1,16 @@
 const sequelize = require("../config/connection");
 
 const models = require('../models');
+const bcrypt = require("bcrypt");
 
 async function run() {
     await models.User.create({
-        name: 'TS',
+        name: 'Bernard Garbol',
+        password: await bcrypt.hash("password", 10),
         profilePhoto: 'https://placekitten.com/300/300',
-        birthDate: '2012-02-04',
+        birthDate: '2000-01-01',
         birthPlace: 'Seattle',
-        email: 'a@example.com',
+        email: 'test@google.com',
         location: 'Seattle,'
     })
 }
