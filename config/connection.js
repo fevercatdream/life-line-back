70102657474772dfb9590c8f7253deb844891709
miri1299 <miri18@gmail.com>
2023-06-05 21:22:10 -0700

    adjust routes to get more data

--- config/connection.js
+++ config/connection.js
@@ -7,9 +7,9 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DB_NAME || "line_db",
+    process.env.DB_USER || "root",
+    process.env.DB_PASSWORD || "",
     {
       host: 'localhost',
       dialect: 'mysql',
