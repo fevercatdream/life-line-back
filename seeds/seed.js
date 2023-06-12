665947a61b6c7535af1c364f47b6cba0b2a9a701
fevercatdream <kuma18@uw.edu>
2023-06-11 17:16:14 -0700

    reseeding the database

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,25 +1,24 @@
 const sequelize = require("../config/connection");
-sequelize.drop()
-//     const models = require('../models');
-//     const bcrypt = require("bcrypt");
+
+const models = require('../models');
+const bcrypt = require("bcrypt");
     
-//     async function run() {    
-//         const pass = await bcrypt.hash("password", 10)
-//         await models.User.bulkCreate([1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(x =>
-//                 (
-//                     {
-//                         name: 'Bernard Garbol '+x,
-//                         password: pass,
-//                         profilePhoto: 'https://placekitten.com/300/300',
-//                         birthDate: '2000-01-01',
-//                         birthPlace: 'Seattle',
-//                         email: `test-${x}@google.com`,
-//                         location: 'Seattle,'
-//                     }
-//                 )
-//             )
-//         )
-//     }
+async function run() {    
+    const pass = await bcrypt.hash("password", 10)
+    await models.User.bulkCreate([1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(x =>
+        (
+            {
+                name: 'Bernard Garbol '+x,
+                password: pass,
+                profilePhoto: 'https://placekitten.com/300/300',
+                birthDate: '2000-01-01',
+                birthPlace: 'Seattle',
+                email: `test-${x}@google.com`,
+                location: 'Seattle,'
+            }
+        )
+    )
+    )
+}
     
-//     run();
-// });
+run();
